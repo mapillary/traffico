ca8cde4fccf6e72cdb8d128da0c3e856a7e0ec45
Kamil Nikel <knikel@posteo.de>
2015-01-22 21:34:56 +0100

    Add utility tasks for parsing CSON

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
 var concat = require('gulp-concat');
+var cson = require('gulp-cson');
+var watch = require('gulp-watch');
 
 gulp.task('clean', shell.task(['rm -f .fontcustom-manifest.json', 'rm -rf ./build/']) );
 
 gulp.task('compile', shell.task('fontcustom compile'));
+
+gulp.task('cson', function() {
+  watch('dev/*.cson')
+    .pipe(cson())
+    .pipe(gulp.dest('build/'))
+});
 
 gulp.task('concat', ['compile'], function () {
   return gulp.src(['build/stylesheets/traffico.css', 'stylesheets/extend.css'])
@@ -12,4 +20,4 @@ gulp.task('concat', ['compile'], function () {
     .pipe(gulp.dest('build/stylesheets'))
 });
 
-gulp.task('default', ['compile', 'concat']);
+gulp.task('default', ['compile', 'concat', 'cson']);

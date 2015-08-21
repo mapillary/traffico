b7b618b9be3daaac97ad12e9ef5b120cce69971a
Kamil Nikel <knikel@posteo.de>
2015-08-21 15:23:55 +0200

    Use feross/standard style, add linting as a gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,55 @@
-var gulp = require('gulp');
-var shell = require('gulp-shell');
-var concat = require('gulp-concat');
-var cson = require('gulp-cson');
-var watch = require('gulp-watch');
-var sass = require('gulp-sass');
+var gulp = require('gulp')
+var shell = require('gulp-shell')
+var concat = require('gulp-concat')
+var cson = require('gulp-cson')
+var sass = require('gulp-sass')
 
-gulp.task('clean', shell.task(['rm -f .fontcustom-manifest.json', 'rm -rf ./build/']));
+gulp.task('lint', shell.task('node node_modules/standard/bin/cmd.js gulpfile.js'))
 
-gulp.task('compile-font', ['clean'], shell.task('fontcustom compile'));
+gulp.task('clean', shell.task(['rm -f .fontcustom-manifest.json', 'rm -rf ./build/']))
 
-gulp.task('cson-signs', ['clean'], function() {
+gulp.task('compile-font', ['clean'], shell.task('fontcustom compile'))
+
+gulp.task('cson-signs', ['clean'], function () {
   return gulp.src('dev/*.cson')
     .pipe(cson())
-    .pipe(gulp.dest('build/signs'));
-});
+    .pipe(gulp.dest('build/signs'))
+})
 
-gulp.task('cson-transformations', ['clean'], function() {
+gulp.task('cson-transformations', ['clean'], function () {
   return gulp.src('stylesheets/transformations.cson')
     .pipe(cson())
     .pipe(gulp.dest('build'))
-});
+})
 
-gulp.task('concat-traffico-css', ['compile-font'], function() {
+gulp.task('concat-traffico-css', ['compile-font'], function () {
   return gulp.src(['build/stylesheets/traffico.css', 'stylesheets/extend.css'])
     .pipe(concat('traffico.css'))
     .pipe(gulp.dest('build/stylesheets'))
-});
+})
 
-gulp.task('gen-overview-css', ['clean'], function() {
-  return gulp.src('stylesheets/examples.scss').pipe(sass()).pipe(gulp.dest('build/stylesheets'));
-});
+gulp.task('gen-overview-css', ['clean'], function () {
+  return gulp.src('stylesheets/examples.scss').pipe(sass()).pipe(gulp.dest('build/stylesheets'))
+})
 
-gulp.task('gen-overview-scss', ['clean'], function() {
-  return gulp.src('stylesheets/examples.scss').pipe(gulp.dest('build/gh-pages'));
-});
+gulp.task('gen-overview-scss', ['clean'], function () {
+  return gulp.src('stylesheets/examples.scss').pipe(gulp.dest('build/gh-pages'))
+})
 
 gulp.task('gen-overview', ['cson-signs', 'cson-transformations'], function () {
-  return gulp.src('scripts/generate-overview.js').pipe(shell(['mkdir -p build/gh-pages && node <%= file.path %>']));
-});
+  return gulp.src('scripts/generate-overview.js').pipe(shell(['mkdir -p build/gh-pages && node <%= file.path %>']))
+})
 
 gulp.task('resolve-transformations', ['cson-signs', 'cson-transformations'], function () {
-  return gulp.src('scripts/resolve-transformations.js').pipe(shell(['mkdir -p build/signs-simple && node <%= file.path %>']));
-});
+  return gulp.src('scripts/resolve-transformations.js').pipe(shell(['mkdir -p build/signs-simple && node <%= file.path %>']))
+})
 
 gulp.task('generate_gh-pages_config', function () {
-  return gulp.src('scripts/generate_gh-pages_config.js').pipe(shell(['mkdir -p build/gh-pages && node <%= file.path %>']));
-});
+  return gulp.src('scripts/generate_gh-pages_config.js').pipe(shell(['mkdir -p build/gh-pages && node <%= file.path %>']))
+})
 
-gulp.task('patch-names', ['gen-overview'], function() {
-  return gulp.src('scripts/patch-names.js').pipe(shell(['node <%= file.path %>']));
-});
+gulp.task('patch-names', ['gen-overview'], function () {
+  return gulp.src('scripts/patch-names.js').pipe(shell(['node <%= file.path %>']))
+})
 
-gulp.task('default', ['concat-traffico-css', 'gen-overview', 'gen-overview-scss', 'gen-overview-css', 'generate_gh-pages_config']);
+gulp.task('default', ['concat-traffico-css', 'gen-overview', 'gen-overview-scss', 'gen-overview-css', 'generate_gh-pages_config'])
